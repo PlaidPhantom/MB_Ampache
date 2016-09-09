59eaf1f20fc9a86e8b392780af11969247119d9d
Sam Shores <plaidphantom@gmail.com>
2016-09-09 12:44:40 -0500

    add test library

---
+++ Tests/AmpacheClientTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicBeePlugin.Ampache;
+using System.Threading;
+
+namespace Tests
+{
+    [TestClass]
+    public class AmpacheClientTests
+    {
+        private AmpacheClient ampacheClient;
+
+        [ClassInitialize]
+        public void Setup()
+        {
+            ampacheClient = new AmpacheClient(new Uri(""), "", "");
+        }
+
+        [TestMethod]
+        public void Do_Handshake()
+        {
+            ManualResetEvent e = new ManualResetEvent(false);
+
+            ampacheClient.HandshakeCompleted += (sender, eventArgs) =>
+            {
+                var result = eventArgs.Result;
+
+                e.Set();
+            };
+
+            ampacheClient.StartHandshake();
+
+            e.WaitOne(5000);
+        }
+    }
+}
